fc2ee519a50d00cde63c7587bbab0f98027477b3
shiki <lotushiki110@163.com>
2020-02-05 22:01:22 +0800

    建立后台调用接口

---
+++ lib/com/tiand/service/service_method.dart
@@ -0,0 +1,32 @@
+//保存所有跟后台请求相关的方法
+import 'package:dio/dio.dart';
+import 'dart:async';
+import 'dart:io';
+import '../config/service_url.dart';
+
+//获取首页主题内容
+Future getHomePageContent() async{
+  try {
+    print('开始获取首页数据');
+    Dio dio = Dio();
+    //发送时候的请求类型  这里使用表单的模式向后端传送数据
+    dio.options.contentType =
+        ContentType.parse("application/x-www-form-urlencoded").toString();
+    //formdata 需要传入经纬度 ?
+    var formData = {};
+    //直接获取 const地址
+    Response response = await dio.post(
+        servicePath['homePageContent'],
+        data: formData
+    );
+    //判断返回
+    if (response.statusCode == 200) {
+      //请求成功
+      return response.data;
+    } else {
+      throw Exception('后端接口访问异常!');
+    }
+  }catch(e){
+    return print('ERROR: =======>${e}');
+  }
+}
