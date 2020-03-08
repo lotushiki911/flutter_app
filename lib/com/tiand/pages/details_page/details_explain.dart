f7703df3e83fe5c70813bcac6b42d90c4d8b6f43
shiki <lotushiki110@163.com>
2020-03-08 19:44:14 +0800

    详情页增加 说明组件

---
+++ lib/com/tiand/pages/details_page/details_explain.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/com/tiand/model/goods_detail_model.dart';
+import 'package:provide/provide.dart';
+import '../../provide/goods_detail_provide.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DetailsExplainArea extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.only(top: 10),
+      width: ScreenUtil().setWidth(700),
+      padding: EdgeInsets.all(10.0),
+      child: Text(
+        '说明: > 好东西 > 正品保证',
+        style: TextStyle(
+          color: Colors.deepOrange,
+          fontSize: ScreenUtil().setSp(30),
+
+        ),
+      ),
+    );
+  }
+}
