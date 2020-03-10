f6857b048c71d5a4c654204cb402e60940ef6467
shiki <lotushiki110@163.com>
2020-03-10 21:16:21 +0800

    购物车主ui编写

--- lib/com/tiand/pages/cart_page.dart
+++ lib/com/tiand/pages/cart_page.dart
@@ -1,81 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/com/tiand/model/cart_info_model.dart';
+import 'package:flutter_app/com/tiand/provide/cart_provide.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provide/provide.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class CartPage extends StatefulWidget {
-  @override
-  _CartPageState createState() => _CartPageState();
-}
 
-class _CartPageState extends State<CartPage> {
-
-  //定义存储数据
-  List<String> testList = [];
-
-
+class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    _show();
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 300,
-            child: ListView.builder(
-                itemCount: testList.length,
-                itemBuilder: (BuildContext context,index){
+
+    return Scaffold(
+      appBar: AppBar(title: Text('购物车'),),
+      body: FutureBuilder(
+        future: _getCartInfo(context),
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            List<CartInfoModel> cartList = Provide.value<CartProvide>(context).cartList;
+            return ListView.builder(
+                itemCount: cartList.length,
+                itemBuilder: (context,index){
                   return ListTile(
-                        title: Text(testList[index]),
+                    title: Text(cartList[index].goodsName),
+                    subtitle: Text(cartList[index].count.toString()),
                   );
-                }
-            ),
-          ),
-          RaisedButton(
-            onPressed: (){_add();},
-            child: Text('增加'),
-          ),
-          RaisedButton(
-            onPressed: (){_clear();},
-            child: Text('清空'),
-          )
-        ],
-      )
+                },
+            );
+          }else{
+            return Text('正在加载中');
+          }
+        },
+      ),
     );
   }
 
-  //处理数据方法 -增加
-  void _add() async{
-      //初始化
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      //定义临时变量
-      String temp = '呵呵哒';
-      //添加数据
-      testList.add(temp);
-      //持久化
-      prefs.setStringList('test1', testList);
-    //查询显示 show
-    _show();
-  }
-
-  //显示 添加的数据
-  void _show() async{
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      if(prefs.getStringList('test1') != null){
-        setState(() {
-          testList = prefs.getStringList('test1');
-        });
-      }
-  }
-
-  //删除
-  void _clear() async{
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      // 1 清除全部的 持久化数据
-  //    prefs.clear();
-      // 2 移除某一个key值
-      prefs.remove('test1');
-      setState(() {
-        testList = [];
-      });
+  //provide必须要上下文才能使用
+  Future<String> _getCartInfo(BuildContext context) async{
+    await Provide.value<CartProvide>(context).getCartInfo();
+    return 'end';
   }
 }
-
