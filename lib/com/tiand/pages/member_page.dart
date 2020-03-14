0aad04d1b004efaee91659948d33fdeebc58b69e
shiki <lotushiki110@163.com>
2020-03-14 21:10:06 +0800

    增加会员中的的编写

--- lib/com/tiand/pages/member_page.dart
+++ lib/com/tiand/pages/member_page.dart
@@ -1,11 +1,164 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MemberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('会员中心'),
+      appBar: AppBar(title: Text('会员中心'),),
+      body: ListView(
+        children: <Widget>[
+          _topHeader(),
+          _orderTitle(),
+          _orderType(),
+          _actionList(),
+        ],
+      ),
+    );
+  }
+
+  //头部
+  Widget _topHeader(){
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      padding: EdgeInsets.all(20),
+      color: Colors.pink,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            width: ScreenUtil().setWidth(200),
+            //头像
+            child: ClipOval(
+                child: Image.network('http://10.0.2.2:5000/static/firstpage/menber/ivanka.jpg')
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              '呵呵哒',
+              style: TextStyle(
+                fontSize: ScreenUtil().setSp(36),
+                color: Colors.black54
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  //我的订单查看页
+  Widget _orderTitle(){
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 1,color: Colors.black12)
+        )
+      ),
+      child: ListTile(
+        leading: Icon(Icons.list),
+        title: Text('我的订单'),
+        trailing: Icon(Icons.arrow_forward),
+      ),
+    );
+  }
+  
+  //
+  Widget _orderType(){
+    return Container(
+      margin: EdgeInsets.only(top: 5),
+      width: ScreenUtil().setWidth(750),
+      height: ScreenUtil().setHeight(150),
+      padding: EdgeInsets.only(top: 10),
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: ScreenUtil().setWidth(185),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.party_mode,
+                  size: 30,
+                ),
+                Text('待付款')
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(185),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.query_builder,
+                  size: 30,
+                ),
+                Text('代发货')
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(185),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.directions_car,
+                  size: 30,
+                ),
+                Text('待收货')
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(185),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.content_paste,
+                  size: 30,
+                ),
+                Text('待评价')
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _actionList(){
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Column(
+          children: <Widget>[
+            _myListTitle('领取优惠卷'),
+            _myListTitle('已领取优惠卷'),
+            _myListTitle('地址管理'),
+            _myListTitle('联系客服'),
+            _myListTitle('关于我们'),
+          ],
+      ),
+    );
+  }
+  //通用listtitle
+  Widget _myListTitle(String title){
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            width: 1,
+            color: Colors.black12
+          )
+        ),
+      ),
+      child: ListTile(
+        leading: Icon(Icons.blur_circular),
+        title: Text(title),
+        trailing: Icon(Icons.keyboard_arrow_right),
       ),
     );
   }
